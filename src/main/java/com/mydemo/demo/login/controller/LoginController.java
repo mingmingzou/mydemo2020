2b1490f703f1c4896f3d179a602a18bed38141d9
爻C <2113896273@qq.com>
2020-08-17 14:57:02 +0800

    首次

--- src/main/java/com/mydemo/demo/login/controller/LoginController.java
+++ src/main/java/com/mydemo/demo/login/controller/LoginController.java
@@ -4,11 +4,11 @@ import com.mydemo.demo.login.entity.PO.Login;
 import com.mydemo.demo.login.service.LoginService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping(value = "/login")
 @Slf4j
 public class LoginController {
